3a197db85b68ac03a205d4f1e6d1c646e26cf25c
Dagmar Lewis <dagmarlewis8@gmail.com>
2022-04-08 13:48:20 -0500

    added reset icon

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-
-
+document.querySelector('i').addEventListener('click', refreshButton)
 
 fetch('https://animechan.vercel.app/api/random')
     .then((response) => response.json())
     .then((quote) => {
-        document.querySelector('h2').innerText=quote.anime
+        document.querySelector('h2').innerText = quote.anime
         document.querySelector('h3').innerText = quote.character
         document.querySelector('p').innerText = quote.quote
     })
+
+function refreshButton() {
+    fetch('https://animechan.vercel.app/api/random')
+        .then((response) => response.json())
+        .then((quote) => {
+            document.querySelector('h2').innerText = quote.anime
+            document.querySelector('h3').innerText = quote.character
+            document.querySelector('p').innerText = quote.quote
+        })
+}
