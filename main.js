c840b7739a365f22ac76f0d4d9836722585e5f7d
Dagmar Lewis <dagmarlewis8@gmail.com>
2022-04-08 00:07:33 -0500

    update files

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+
+fetch('https://animechan.vercel.app/api/random')
+    .then((response) => response.json())
+    .then((quote) => {
+        document.querySelector('h2').innerText=quote.anime
+        document.querySelector('h3').innerText = quote.character
+        document.querySelector('p').innerText = quote.quote
+    })
